04ae6b0f2f261b7b8c15199fb0497db888692032
IT21109508 <99158335+IT21109508@users.noreply.github.com>
2022-05-20 21:46:30 +0530

    Create Seller.cpp

---
+++ Seller.cpp
@@ -0,0 +1,23 @@
+#include "Seller.h"
+
+Seller::Seller(){
+	
+}
+
+Seller::Seller(const char selluname[],const char sellpword[],int sellid, 
+				const char sellname[], const char selladdress[] , const char sellmail[], const char selltno[]):RegisteredUser(selluname,sellpword,sellid,sellname,
+				selladdress,sellmail,selltno){
+	
+}
+
+void Seller::login(){
+	
+}
+
+void Seller::displaySellerDetails(){
+	
+}
+
+Seller::~Seller(){
+	
+}
