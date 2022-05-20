6c5ee3e7ca8878abdd238ac35bf4d141caade8d6
IT21109508 <99158335+IT21109508@users.noreply.github.com>
2022-05-20 21:46:52 +0530

    Create Buyer.cpp

---
+++ Buyer.cpp
@@ -0,0 +1,23 @@
+#include "Buyer.h"
+
+Buyer::Buyer(){
+	
+}
+
+Buyer::Buyer(const char buyuname[],const char buypword[],int buyid, 
+				const char buyname[], const char buyaddress[] , const char buymail[], const char buytno[]):RegisteredUser(buyuname,buypword,buyid,buyname,
+				buyaddress,buymail,buytno){
+	
+}
+
+void Seller::login(){
+	
+}
+
+void Seller::displayBuyerDetails(){
+	
+}
+
+Buyer::~Buyer(){
+	
+}
